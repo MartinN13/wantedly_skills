854ff56eea74c9d133fd6d09e3d359bf3d9a2fc8
MartinN13 <martinnabhan@me.com>
2017-05-24 00:04:50 +0900

    Add skill and user_skill models, create model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
+  has_many :user_skills
+  has_many :skills, through: :user_skills
+
+  validates :name, presence: true
+  validates :email, presence: true
+
   has_secure_password
 end
