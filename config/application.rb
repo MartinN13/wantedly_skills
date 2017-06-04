cb32252c7d7d3a79ebe0dd18ff29e654eed9efa3
MartinN13 <martinnabhan@me.com>
2017-06-05 00:30:09 +0900

    Eager load paths to fix token auth in production

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,6 @@ Bundler.require(*Rails.groups)
 module WantedlySkills
   class Application < Rails::Application
     config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end
