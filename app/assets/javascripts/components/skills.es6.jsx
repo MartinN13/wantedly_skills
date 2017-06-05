7a449531390cc8b60098c02d8150eb92e0d61483
MartinN13 <martinnabhan@me.com>
2017-06-05 14:08:23 +0900

    Add toast messages when skill is added or removed

--- app/assets/javascripts/components/skills.es6.jsx
+++ app/assets/javascripts/components/skills.es6.jsx
@@ -3,10 +3,12 @@ class Skills extends React.Component {
     super(props);
     this.state = {
       name: '',
+      message: ''
     }
     
     this.addSkill = this.addSkill.bind(this);
     this.handleName = this.handleName.bind(this);
+    this.handleMessage = this.handleMessage.bind(this);
   }
   addSkill() {
     $.ajax({
@@ -20,24 +22,32 @@ class Skills extends React.Component {
         name: this.state.name,
       },
       success: result => {
-        console.log(result.responseText);
-        location.reload();
+        this.setState({name: ''});
+        this.setState({message: result.responseText});
       },
       error: result => {
         console.error(result.responseText);
+        this.setState({message: result.responseText});
       }
     });
   }
   handleName(e) {
     this.setState({name: e.target.value});
-    console.log(e.target.value);
+  }
+  handleMessage() {
+    this.setState({message: ''});
   }
   render () {
     return (
       <div className="content">
-        <Navbar />
         <div className="column col-12">
           <h1>Add skill</h1>
+          {this.state.message && 
+            <div className="toast">
+              <button className="btn btn-clear float-right" onClick={this.handleMessage}></button>
+              {this.state.message}
+            </div>
+          }
           <div className="form-group">
               <label className="form-label">Name</label>
               <input className="form-input" type="text" placeholder="Name" value={this.state.name} onChange={this.handleName}></input>
